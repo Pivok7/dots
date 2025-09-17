2bd3fabc4a4042bba1e6e8a583ae390db095a746
Pivok <pivoc@protonmail.com>
2025-09-17 16:34:15 +0200

    Todo

--- hypr-desktop/eww/scripts/audio/buffer_mic.sh
+++ hypr-desktop/eww/scripts/audio/buffer_mic.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-case "$1" in
-    "up")
-	eww update mic_vol=$(($(eww get mic_vol)+1))
-    ;;
-    "down")
-	eww update mic_vol=$(($(eww get mic_vol)-1))
-    ;;
-    *) exit 1 ;;
-esac
-
 echo "$1" "1" >> /tmp/eww_mic.tmp
--- hypr-desktop/eww/scripts/audio/buffer_vol.sh
+++ hypr-desktop/eww/scripts/audio/buffer_vol.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-case "$1" in
-    "up")
-	eww update volume=$(($(eww get volume)+1))
-    ;;
-    "down")
-	eww update volume=$(($(eww get volume)-1))
-    ;;
-    *) exit 1 ;;
-esac
-
 echo "$1" "1" >> /tmp/eww_vol.tmp

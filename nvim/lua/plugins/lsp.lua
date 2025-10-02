d889d8075117f693c0886dffbe88d3536fb7d3e1
Pivok <pivoc@protonmail.com>
2025-10-02 17:46:12 +0200

    Up

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,18 +1,15 @@
 return {
-    {
-        'mason-org/mason.nvim',
-        config = function()
-            require('mason').setup()
-        end
+    "mason-org/mason-lspconfig.nvim",
+
+    dependencies = {
+	{
+	    "mason-org/mason.nvim",
+	    opts = {}
+	},
+	{ "neovim/nvim-lspconfig", },
     },
-    {
-	"mason-org/mason-lspconfig.nvim",
-	opts = {
-	    ensure_installed = { "lua_ls" },
-	},
-	dependencies = {
-	    { "mason-org/mason.nvim", opts = {} },
-	    "neovim/nvim-lspconfig",
-	},
-    }
+
+    opts = {
+	ensure_installed = { "lua_ls" },
+    },
 }
